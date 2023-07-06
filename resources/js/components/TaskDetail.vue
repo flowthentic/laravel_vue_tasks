3c3f1ba08d7de6ec114be6bf766e1c2c57754b6a
Jakub <16520169+flowthentic@users.noreply.github.com>
2023-07-06 14:14:24 +0200

    Basic form validation and error message

--- resources/js/components/TaskDetail.vue
+++ resources/js/components/TaskDetail.vue
@@ -1,56 +1,80 @@
-<script setup>
-import { inject } from 'vue';
-const selectedTask = inject('selectedTask');
-const updateTask = inject('updateTask');
-
-const onUpdated = function(){
-alert();
-    updateTask()
-}
-</script>
 
 <template>
-    <label>Názov</label>
-    <input type="text" name="name" v-model="selectedTask.name" required>
-    <!--input type="text" name="name" :value="selectedTask.name" v-on:input="updateTask(selectedTask)" required-->
-    <label>Popis</label>
-    <input type="textarea" name="description" v-model="selectedTask.description" required>
-    <label>Termín</label>
-    <input type="date" name="due" v-model="selectedTask.due" required>
-    <label>Splnený</label>
-    <input type="checkbox" name="complete" v-model="selectedTask.completed">
+    <form ref="fields">
+        <label>Názov</label>
+        <input type="text" name="name" v-model="selectedTask.name" required>
+        <label>Popis</label>
+        <input type="textarea" name="description" v-model="selectedTask.description">
+        <label>Termín</label>
+        <input type="date" name="due" v-model="selectedTask.due" required>
+        <label>Splnený</label>
+        <input type="checkbox" name="complete" v-model="selectedTask.completed">
+    </form>
+    <em id="error">{{message}}</em>
 </template>
 
 <script>
+import { inject } from 'vue';
 export default {
+    setup: function() {
+        const selectedTask = inject('selectedTask');
+        return {selectedTask};
+    },
+    data: function() {
+        return {
+            message: ''
+        }
+    },
     methods: {
-        save: function() {
-            axios.post('/task',{
-                name: this.newtask,
-                description: this.task.description,
+        saveTask: function() {
+
+        },
+        updateTask: function() {
+            axios.patch('/task/' + this.update_task.id, {
+                name: this.update_task.name,
+                description: this.update_task.description,
             })
             .then(response => {
-                this.reset();
-                this.tasks.push(response.data.task);
-                $("#add_task_model").modal("hide");
+                $("#update_task_model").modal("hide");
             })
             .catch(error => {
                 this.errors = [];
-                if (error.response.data.errors && error.response.data.errors.name) {
+                if (error.response.data.errors.name) {
                     this.errors.push(error.response.data.errors.name[0]);
                 }
-                if (error.response.data.errors && error.response.data.errors.description) {
+                if (error.response.data.errors.description) {
                     this.errors.push(error.response.data.errors.description[0]);
                 }
             });
         },
-        onUpdated: function(){
-alert();
-    updateTask()
-}
+        logError: function(error) {
+            this.errors = [];
+            if (error.response.data.errors.name) {
+                this.errors.push(error.response.data.errors.name[0]);
+            }
+            if (error.response.data.errors.description) {
+                this.errors.push(error.response.data.errors.description[0]);
+            }
+        }
     },
     mounted() {
 
+    },
+    created: function(){
+        const saver = setInterval((vcomponent) => {
+
+            this.message = '';
+            if (!vcomponent.$refs.fields.checkValidity())
+                this.message = 'Form is invalid';
+            else if (!this.selectedTask.unsaved); //pass
+            else if (this.selectedTask.id)
+                axios.patch('/task/' + this.selectedTask.id, this.selectedTask)
+                .then((response) => this.selectedTask = response.data.task)
+                .catch(this.logError);
+            else axios.post('/task/', this.selectedTask)
+                .then((response) => this.selectedTask = response.data.task)
+                .catch((error) => this.message = error.response.data.errors.name[0]);
+        }, 1000, this);
     }
 };
 </script>
